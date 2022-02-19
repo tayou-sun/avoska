49d5ffdbf3eaa3431fb6d59a0781234e3ca31d1e
Your Name <you@example.com>
2022-02-19 22:28:49 +0300

    change chat id

--- Avoska.Web/Controllers/OrderController.cs
+++ Avoska.Web/Controllers/OrderController.cs
@@ -34,18 +34,18 @@ namespace Avoska.Web.Controllers
             var bot = new TelegramService();
             bot.SendMessage(order, res);
 
-
-       /*      var client = new RestClient("http://api.callmebot.com/start.php?source=web&user=@shainurova_e&text=hello%20everyone&lang=en-US-Standard-B");
+/*
+          var client = new RestClient("http://api.callmebot.com/start.php?source=web&user=@shainurova_e&text=hello%20everyone&lang=en-US-Standard-B");
 
             var request = new RestRequest();
             request.AddHeader("Authorization", "Basic cGFydHk6cGFycm90");
             var response =  client.ExecuteAsync(request);
-
+    */
             var client1 = new RestClient("http://api.callmebot.com/start.php?source=web&user=@eugenemavrin&text=hello%20everyone&lang=en-US-Standard-B");
             var request1 = new RestRequest();
             request1.AddHeader("Authorization", "Basic cGFydHk6cGFycm90");
             var response1 =  client1.ExecuteAsync(request1);
- */
+
 
 
             //Console.WriteLine(response.Content);
